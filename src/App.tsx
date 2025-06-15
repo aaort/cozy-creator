b1a2079ad31daaa7d1604f0f1f32957d1fcee439
aaort <artashesano@gmail.com>
2025-06-15 10:29:17 +0400

    fix grid behavior to provide proper initial spacing between header and grid and allow content to be scrolled under the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import { Header } from "@components/layout/header";
 import { VirtualizedImageGrid } from "@components/ui/virtualized-image-grid";
 import { STATIC_IMAGE_URL } from "@constants/data";
+import { useState } from "react";
+import type { GridOnScrollProps } from "react-window";
+import { HEADER_HEIGHT } from "./constants/layout";
 
 function App() {
+  const [space, setSpace] = useState<number>(HEADER_HEIGHT);
+
+  const handleGridScroll = (props: GridOnScrollProps) => {
+    setSpace(Math.max(HEADER_HEIGHT - props.scrollTop, 0));
+  };
+
   return (
     <div className="min-h-screen bg-background text-foreground">
       <Header />
 
-      <main className="px-2 pb-8">
+      <main className="px-2" style={{ paddingTop: space }}>
         <VirtualizedImageGrid
           gap={36}
           itemHeight={280}
           itemsPerPage={40}
           className="mx-auto"
           initialItemCount={40}
+          onGridScroll={handleGridScroll}
           baseImageUrl={STATIC_IMAGE_URL}
         />
       </main>
